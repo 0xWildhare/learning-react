ce1cd78939e749ebb86db3ca2de0b17394cb3275
0xWildhare <sschell1979@gmail.com>
2022-03-19 18:56:04 -0700

    working project

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,19 @@ class SearchBar extends React.Component {
 
   state = { term: '' }
 
-onInputChange = (e) => {
-  this.setState({ term: e.target.value });
-  console.log(this.state.term);
-};
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+  onFormSubmit = (e) => {
+    //default is to clear the form and refresh page, so...
+    e.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  }
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
