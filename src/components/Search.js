0c92f62705a79fb335788bc24a5ee447fa95d23b
0xWildhare <sschell1979@gmail.com>
2022-03-24 21:27:49 -0700

    finished search widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('genesis');
+  const [results, setResults] = useState([]);
+
 
   useEffect(() => {
-    (async () => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
@@ -15,8 +17,43 @@ const Search = () => {
           srsearch: term
         },
       });
-    })();
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+        const timeoutId = setTimeout(() => {
+            if(term) search();
+        },500);
+
+        return () => {
+          clearTimeout(timeoutId);
+    };
+  }
+
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -30,6 +67,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
