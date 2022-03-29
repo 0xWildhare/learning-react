1dc8a9cd0301eeb7ed676956ab55cfea8770027c
0xWildhare <sschell1979@gmail.com>
2022-03-28 21:25:33 -0700

    added translate widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
-import Search from './components/Search'
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -17,11 +19,43 @@ const items = [
 }
 ]
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'a shade of blue',
+    value: 'blue'
+  }
+]
+
 export default () => {
+  //const [selected,setSelected] = useState(options[0]);
+  //const [showDropdown, setShowDropdown] = useState(true);
+
   return(
     <div>
       {/*<Accordion items={items}/>*/}
-      <Search />
+      {/*<Search />*/}
+      {/*<button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+      {showDropdown ?
+        <Dropdown
+          selected={selected}
+          options={options}
+          onSelectedChange={setSelected}
+        /> : null
+      }
+      <div>
+        <h1>
+          <font color={selected.value}>{`the color is ${selected.value}`}</font>
+        </h1>
+      </div>*/}
+      <Translate />
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,8 +3,17 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('genesis');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    },500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  });
 
   useEffect(() => {
     const search = async () => {
@@ -14,25 +23,14 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
+          srsearch: debouncedTerm
         },
       });
       setResults(data.query.search);
     };
+    search();
+  }, [debouncedTerm]);
 
-    if (term && !results.length) {
-      search();
-    } else {
-        const timeoutId = setTimeout(() => {
-            if(term) search();
-        },500);
-
-        return () => {
-          clearTimeout(timeoutId);
-    };
-  }
-
-  }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
